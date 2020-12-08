90f3c5c51de872096ef62589c034da43ccff8b9d
developbio <70217967+developbio@users.noreply.github.com>
2020-12-08 11:58:58 -0500

    Update download_data.sh

--- bash_scripts/download_data.sh
+++ bash_scripts/download_data.sh
@@ -5,9 +5,9 @@ set -e
 mkdir -p ../data
 
 # Check to make sure years are correct
-if [ "$1" -lt 1983 -o "$3" -gt $(($(date +%Y) - 1)) -o "$1" -gt "$3"] 
+if [ "$1" -lt 1969 -o "$3" -gt $(($(date +%Y) - 1)) -o "$1" -gt "$3"] 
 then
-    echo "Accessible CDC data only goes back until 1983. Please choose another starting year."
+    echo "Accessible CDC data only goes back until 1969. Please choose another starting year."
     echo "The most current data is from 2019. CDC birth data for year Y usually comes out 6-12 months after the end of that calendar year. Please choose another ending year."
     echo "Ensure the arguments are in order: start_year, increment, end year."
     exit 1
